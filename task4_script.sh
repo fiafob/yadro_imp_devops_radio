09db35abb99e292f27688dbe91ce8b7c2c6329ce
fiafob <78419078+fiafob@users.noreply.github.com>
2025-05-06 18:47:34 +0300

    Комментарии к task4_script.sh

--- task4_script.sh
+++ task4_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $# -le 1 ]
+if [ $# -le 1 ] # Проверка наличия аргументов
 then
-	echo "Script usage: ./task4_script.sh [substring] [file]"
+	# Вывод информации по использованию скрипта, в случае, если передано менее 2 аргументов
+	echo "Script usage: ./task4_script.sh substring file"
 else
-
 	grep "$1" $2 --color=auto
 fi
 
